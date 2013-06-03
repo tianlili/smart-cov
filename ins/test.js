159675024cfd47bd8b38c70db58572b4809da3d8
tianlili <tianlili3283@gmail.com>
2013-06-03 13:51:15 +0800

    [Change]删除无用文件

--- ins/test.js
+++
@@ -1,6 +0,0 @@
-if (typeof __coverage__ === 'undefined') { __coverage__ = {}; }
-if (!__coverage__['D:\\Workplace\\smart-cov\\ins\\test.js']) {
-   __coverage__['D:\\Workplace\\smart-cov\\ins\\test.js'] = {"path":"D:\\Workplace\\smart-cov\\ins\\test.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}};
-}
-var __cov_Ig2lRpXGSa$7NLQSovZPnA = __coverage__['D:\\Workplace\\smart-cov\\ins\\test.js'];
-
