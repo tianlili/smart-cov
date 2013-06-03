cb2ba97a423b522139eff234d317a710b48cd1e1
tianlili <tianlili3283@gmail.com>
2013-06-03 17:30:21 +0800

    [change]修改版本号

--- smart-cov.js
+++ smart-cov.js
@@ -1,11 +1,11 @@
-void function (version, script, analyzer, parent) {
+void function (version, scripts, analyzer, parent) {
     if (window != window.parent)
         return;
 
     if (/msie/i.test(navigator.userAgent))
         return window.alert("您使用的浏览器相对传统，建议换 chrome/firefox/safari 试试！");
 
-    version = "1.1.0";
+    version = "1.1.1";
 
     scripts = [];
     scripts[0] = "http://127.0.0.1/smart-cov/cache.php?file=./esprima.js";
